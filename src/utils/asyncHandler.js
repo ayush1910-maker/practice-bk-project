f2b53be0052894411acca4a417c128878bd0e4b4
Ayush Porwal <ayushporwal3619@gmail.com>
2025-05-25 12:53:48 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,40 @@
+// ++++++++++++++++++++++++ PROMISE METHOD +++++++++++++++++++++
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err)) 
+    }
+}
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// +++++++++++++++++++++++++++ TRY CATCH METHOD ++++++++++++++++++++++++++
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fn)  => {()=> {}}
+// const asyncHandler = (fn) => async() => {}
+
+
+// const asyncHandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }

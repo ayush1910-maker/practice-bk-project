5023fe7cfce1be8e03e71f60b607728c62dcda7d
Ayush Porwal <ayushporwal3619@gmail.com>
2025-06-02 10:40:42 +0530

    make a file upload system for project

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp')
+  },
+  filename: function (req, file, cb) {
+    // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) 
+    cb(null, file.originalname)  // kya pta ayush name ki five files aa jaye so it's not a good thing
+  }
+})
+
+export const upload = multer({
+     storage,
+})

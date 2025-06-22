a18f79b2de60fba495a5020e41146f569a4581c6
Ayush Porwal <ayushporwal3619@gmail.com>
2025-06-22 17:42:30 +0530

    comment in app.js and resolve a error in user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose ,{Schema} from "mongoose";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
-import { Schema } from "mongoose";
+// import { Schema } from "mongoose";
 
 const userSchema = new Schema(
     {

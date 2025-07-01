75d63ee398f4dcc95708bbaa0816696a2d5de9f5
Ayush Porwal <ayushporwal3619@gmail.com>
2025-07-01 16:52:07 +0530

    changes in cloudinary.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -6,6 +6,7 @@ const storage = multer.diskStorage({
     cb(null, "./public/temp")
   },
 
+  
   filename: function (req, file, cb) {
     // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) 
     cb(null, file.originalname)  // kya pta ayush name ki five files aa jaye so it's not a good thing

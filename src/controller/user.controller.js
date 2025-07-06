9d6a05cb520bca69259ff62735e3875a37d8e5af
Ayush Porwal <ayushporwal3619@gmail.com>
2025-07-06 12:28:00 +0530

    create loginuser controller and fix a  small typo

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,6 +4,24 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary} from "../utils/cloudinary.js"
 import { Apiresponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+       const user =  await User.findById(userId)
+       const accessToken = user.generateAccessToken()
+       const refreshToken = user.generateRefreshToken()
+
+       user.refreshToken = refreshToken
+       await user.save({ validateBeforeSave: false })
+
+       return {accessToken , refreshToken}
+
+
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while genereating refresh and access token")
+    }
+}
+
+
 const registerUser = asyncHandler( async (req,res) => {
     // get user details from frontend
     // validation - not empty
@@ -16,7 +34,7 @@ const registerUser = asyncHandler( async (req,res) => {
     // retrun res
 
    const {fullName,email,username,password} = req.body
-   console.log("email: ", email);
+//    console.log("email: ", email);
 
 //    if(fullname === ""){
 //     throw new ApiError(400, "fullname is required")
@@ -25,8 +43,8 @@ const registerUser = asyncHandler( async (req,res) => {
 
 //  advance methods
 if (
-    [fullName, email, username, password].some((field)=>
-        field?.trim() === "")
+    [fullName, email, username, password].some((feild)=>
+        feild?.trim() === "")
 ) {
     throw new ApiError(400, "All feilds are required")
 }
@@ -89,4 +107,72 @@ if(!avatar){
 
 } )
 
-export { registerUser }
+const loginUser = asyncHandler( async (req , res) => {
+
+    // get data from frontend
+    // validation
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookie
+    // return res
+
+    // 1 
+    const {email,username,password} = req.body
+
+
+    // 2
+    if(!username || !email ){
+        throw new ApiError(400, "email or  username are required")
+    }
+
+    // 3
+    const user = User.findOne({
+        $or: [{username} , {email}]
+    })
+
+    if (!user) {
+        throw new ApiError(404 , "User does not exist")
+    }
+
+    // 4
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(400, "password are required")
+    }
+
+    // 5
+    const {accessToken , refreshToken} = await
+     generateAccessAndRefreshTokens(user._id)
+
+    // 6
+    const loggedInUser = await User.findById(user._id).
+    select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken", refreshToken,options)
+    .json(
+        new Apiresponse(
+            200,
+            {
+                user: loggedInUser , accessToken,
+                refreshToken
+            },
+            "User logged In successfully"
+        )
+    )
+
+})
+
+
+export { 
+    registerUser,
+    loginUser 
+}

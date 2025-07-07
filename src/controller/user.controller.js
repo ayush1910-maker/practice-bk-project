a35d8619950189b952b6d5e7d42406958def7e71
Ayush Porwal <ayushporwal3619@gmail.com>
2025-07-07 17:50:59 +0530

    add  authMiddleware and login logout controllers

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -139,17 +139,17 @@ const loginUser = asyncHandler( async (req , res) => {
     const isPasswordValid = await user.isPasswordCorrect(password)
 
     if(!isPasswordValid){
-        throw new ApiError(400, "password are required")
+        throw new ApiError(400, "password are invalid")
     }
 
     // 5
     const {accessToken , refreshToken} = await
      generateAccessAndRefreshTokens(user._id)
 
+     const loggedInUser = await User.findById(user._id).
+     select("-password -refreshToken")
+     
     // 6
-    const loggedInUser = await User.findById(user._id).
-    select("-password -refreshToken")
-
     const options = {
         httpOnly: true,
         secure: true
@@ -171,8 +171,35 @@ const loginUser = asyncHandler( async (req , res) => {
 
 })
 
+const logoutUser = asyncHandler(async(req , res) => {
+    User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new Apiresponse(200 , {} , "User logged Out"))
+
+
+})
 
 export { 
     registerUser,
-    loginUser 
+    loginUser,
+    logoutUser
 }

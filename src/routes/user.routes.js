e60634dc71ec3d61f6dc71c3478173c7db99f2bd
Ayush Porwal <ayushporwal3619@gmail.com>
2025-07-16 16:41:14 +0530

    some fixex

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controller/user.controller.js";
+import { loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } from "../controller/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js" 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
